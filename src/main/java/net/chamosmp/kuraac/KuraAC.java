56cccf363ac9badaa9b329b5233580261e14d02f
Chamogelastos <stavros.ssta@gmail.com>
2026-02-06 20:08:09 +0200

    I hate gradle

---
+++ src/main/java/net/chamosmp/kuraac/KuraAC.java
@@ -0,0 +1,17 @@
+package net.chamosmp.kuraac;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KuraAC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
