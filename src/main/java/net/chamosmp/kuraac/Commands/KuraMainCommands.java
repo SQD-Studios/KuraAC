002f363a3f748f0f8e8cecf266890c231c19049c
Chamogelastos <stavros.ssta@gmail.com>
2026-02-28 12:33:05 +0200

    Added bStats metrics

--- src/main/java/net/chamosmp/kuraac/Commands/KuraMainCommands.java
+++ src/main/java/net/chamosmp/kuraac/Commands/KuraMainCommands.java
@@ -8,9 +8,10 @@ public class KuraMainCommands {
 
     public static void register(Commands commands) {
         commands.register(Commands.literal("kuraac")
-                        .then(Commands.literal("kuraac help"))
+                        .then(Commands.literal("help"))
+                        .requires(sender -> sender.getSender().hasPermission("kuraac.*"))
                         .executes(ctx ->{
-                            ctx.getSource().getSender().sendPlainMessage("KuraAC Built To Perform. Do /kuraac help for more info!");
+                            ctx.getSource().getSender().sendPlainMessage("Here is the list of commands:");
                             return Command.SINGLE_SUCCESS;
                         })
 
@@ -20,10 +21,8 @@ public class KuraMainCommands {
                     return Command.SINGLE_SUCCESS;
 
                 })
-                .build());
+                .build()); // WE ARE THEMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
     }
 
 }
-
-
 
